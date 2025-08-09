c282481407d0acbf99062f4a916c5d5b22ba2203
老板1678 <410717263@qq.com>
2025-08-10 02:59:31 +0800

    Changes

--- app.py
+++ app.py
@@ -28,7 +28,6 @@ def get_song(word):
     song_url_lst = []
     try:
         co = ChromiumOptions()
-        co.set_proxy('socks5://127.0.0.1:10808')
         co.set_argument('referer', "https://www.douyin.com/")
         co.set_user_agent(
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36')
