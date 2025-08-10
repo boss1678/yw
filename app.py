8699b202f7201f0791f1ee283d3277d72ac6b307
老板1678 <410717263@qq.com>
2025-08-10 19:47:45 +0800

    Changes

--- app.py
+++ app.py
@@ -25,20 +25,72 @@ def get_song(word):
     歌曲下载
     :return:
     """
-    headers = {
-        'referer': 'https: // www.shicimingju.com /',
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
-    }
-    pinyin_list = lazy_pinyin(word)
-    pinyin_str = ''.join(pinyin_list)
-    url = f'https://www.shicimingju.com/book/{pinyin_str}.html'
-    resp = requests.get(url, headers=headers)
-    resp.encoding = 'utf-8'
-    soup = BeautifulSoup(resp.text, 'html.parser')
-    urls = [sp.get_text() for sp in soup.find('div', class_="list").find_all('a')]
-    return {
-        'urls': urls
-    }
+    # headers = {
+    #     'referer': 'https: // www.shicimingju.com /',
+    #     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
+    # }
+    # pinyin_list = lazy_pinyin(word)
+    # pinyin_str = ''.join(pinyin_list)
+    # url = f'https://www.shicimingju.com/book/{pinyin_str}.html'
+    # resp = requests.get(url, headers=headers)
+    # resp.encoding = 'utf-8'
+    # soup = BeautifulSoup(resp.text, 'html.parser')
+    # urls = [sp.get_text() for sp in soup.find('div', class_="list").find_all('a')]
+    # return {
+    #     'urls': urls
+    # }
+    song_urls = []
+    co = ChromiumOptions()
+    co.headless(True)
+    co.set_user_agent(
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36')
+    page = ChromiumPage()
+    url = f'https://www.douyin.com/search/{word}?type=general'
+    if url:
+        print(f'准备下载 --> {word}....')
+    page.listen.start('general/search')
+    page.get(url)
+    page.wait(2)
+    while 1:
+        page.run_js('window.scrollBy(0, 1200)')
+        page.wait(3)
+        if page.run_js(
+                'return (window.innerHeight + document.scrollingElement.scrollTop) >= document.scrollingElement.scrollHeight - 10'):
+            break
+        try:
+            while 1:
+                res = page.listen.wait(timeout=0.5)
+                if not res or isinstance(res, bool):
+                    break
+                resp = res.response.body
+                if not isinstance(resp, dict):
+                    continue
+
+                for i in resp.get('data'):
+                    if not i:
+                        continue
+                    aweme_info = i.get('aweme_info')
+                    if not aweme_info:
+                        continue
+                    video = aweme_info.get('video')
+                    if not video:
+                        continue
+                    play_addr = video.get('play_addr')
+                    if not play_addr:
+                        continue
+                    url_list = play_addr.get('url_list')
+                    if not url_list:
+                        continue
+                    song_url = url_list[-1]
+                    song_urls.append(song_url)
+                    if len(song_urls) >= 5:
+                        break
+            page.quit()
+            return {
+                'song_urls': song_urls
+            }
+        except Exception as e:
+            print(e)
 
 
 @app.route('/')
@@ -57,5 +109,5 @@ def song():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-    # print(get_song('三国演义'))
+    # app.run(host='0.0.0.0', port=5000, debug=True)
+    print(get_song('天际'))
